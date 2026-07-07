1c57ce2e91a60138335b31e43502f7b4ceb84938
NguyenQuocKhanh0 <108500182+NguyenQuocKhanh0@users.noreply.github.com>
2026-07-07 20:47:12 +0700

    Update gen.py

--- gen.py
+++ gen.py
@@ -13,7 +13,7 @@ prompt_wav_path = "tin_nhanh.wav"
 prompt_text = "đến tiệm mua chai thuốc trừ sâu người đàn ông hỏi một câu khiến nhân viên giật mình lấy lại chai thuốc."
 inference_timesteps = 64
 
-NUM_SAMPLES = 1000
+NUM_SAMPLES = 6500
 
 # ==========================
 # Tạo thư mục output
